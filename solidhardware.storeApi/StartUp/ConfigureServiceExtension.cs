01478a02989d920f0e0beb56905619d1f32eacdb
OsamaMousa11 <osamaomar1995@gmail.com>
2025-10-18 00:16:57 +0300

    Add project files.

---
+++ solidhardware.storeApi/StartUp/ConfigureServiceExtension.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace solidhardware.storeApi.StartUp
+{
+    public static class ConfigureServiceExtension
+    {
+        public static IServiceCollection ServiceConfiguration(this IServiceCollection Services, IConfiguration Configuration)
+        {
+           /* Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("connstr") ??
+                throw new InvalidOperationException("Connection string 'connstr' not found."));
+            });*/
+
+           /* Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequiredUniqueChars = 5;
+            })
+              .AddEntityFrameworkStores<AppDbContext>()
+              .AddDefaultTokenProviders()
+              .AddUserStore<UserStore<ApplicationUser, ApplicationRole, AppDbContext, Guid>>()
+              .AddRoleStore<RoleStore<ApplicationRole, AppDbContext, Guid>>();
+            Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+               .AddJwtBearer(o =>
+               {
+                   o.RequireHttpsMetadata = false;
+                   o.SaveToken = false;
+                   o.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuerSigningKey = true,
+                       ValidateIssuer = true,
+                       ValidateAudience = true,
+                       ValidateLifetime = true,
+                       ValidIssuer = Configuration["JWT:Issuer"],
+                       ValidAudience = Configuration["JWT:Audience"],
+                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Key"])),
+                       ClockSkew = TimeSpan.Zero
+                   };
+               });
+            Services.Configure<DataProtectionTokenProviderOptions>(options =>
+            {
+                options.TokenLifespan = TimeSpan.FromHours(1);
+            });
+            Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+                });
+            });
+
+
+            Services.Configure<JwtDTO>(Configuration.GetSection("JWT"));
+            Services.Configure<MailSettings>(Configuration.GetSection("MailSettings"));
+            Services.AddTransient<IMailingService, MailingService>();
+
+   */
+
+
+
+
+          //  Services.AddAutoMapper(typeof(EmployeeConfig).Assembly);
+            Services.AddEndpointsApiExplorer();
+            Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Employee Management ",
+                    Version = "v1",
+                    Description = "Employee Management API"
+                });
+            });
+
+
+            return Services;
+        }
+    }
+}
